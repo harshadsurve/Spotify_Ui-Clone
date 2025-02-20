38d8226b41d1d99f53991d82b682ee6125be2450
harshad <harshadsurve2003@gmail.com>
2025-02-20 16:03:09 +0530

    Spotify-Ui-Clone File Setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="text-primary">Welcome to My React App</h1>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
